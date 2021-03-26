f504ae07fb038feebf91af28c859aa954d690acd
Gaming32 <soujournme@gmail.com>
2021-03-26 08:54:01 -0500

    added "load" opcodes

--- eight_bit/ops.py
+++ eight_bit/ops.py
@@ -6,4 +6,52 @@ def nop(comp: Computer):
     pass
 
 
-opcodes: list[Callable[[Computer], Any]] = [nop]
+def ldx_imm(comp: Computer):
+    comp.pc += 1
+    data = comp.getat(comp.pc)
+    comp.regx = data
+
+
+def ldx(comp: Computer):
+    comp.pc += 1
+    ptr = comp.getat(comp.pc)
+    data = comp.getat(ptr)
+    comp.regx = data
+
+
+def ldy_imm(comp: Computer):
+    comp.pc += 1
+    data = comp.getat(comp.pc)
+    comp.regy = data
+
+
+def ldy(comp: Computer):
+    comp.pc += 1
+    ptr = comp.getat(comp.pc)
+    data = comp.getat(ptr)
+    comp.regy = data
+
+
+def lda_imm(comp: Computer):
+    comp.pc += 1
+    data = comp.getat(comp.pc)
+    comp.accum = data
+    
+
+def lda(comp: Computer):
+    comp.pc += 1
+    ptr = comp.getat(comp.pc)
+    data = comp.getat(ptr)
+    comp.accum = data
+
+
+opcodes: list[Callable[[Computer], Any]] = [
+    nop,
+
+    ldx_imm,
+    ldx,
+    ldy_imm,
+    ldy,
+    lda_imm,
+    lda
+]
